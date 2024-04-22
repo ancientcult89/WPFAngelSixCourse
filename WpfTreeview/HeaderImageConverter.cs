25307727aca97164fce46054f17e3b1673a36726
PolyakovSergey <ancientcult89@gmail.com>
2024-04-22 20:37:09 +0700

    lesson 2

---
+++ WpfTreeview/HeaderImageConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace WpfTreeview
+{
+    [ValueConversion(typeof(string), typeof(BitmapImage))]
+    public class HeaderImageConverter : IValueConverter
+    {
+        public static HeaderImageConverter Instance = new HeaderImageConverter();
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string path = (string)value;
+
+            if(path == null)
+                return null;
+
+            var name = MainWindow.GetFileFolderName(path);
+
+            string image = "Images/HardDrive.png";
+
+            if (path.Length <= 4) //инкастыляция, по уроку сюда ничего не должно было падать и так определялся драйв...
+                image = "Images/HardDrive.png";
+            else if(new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
+                image = "Images/ClosedFolder.png";
+            else
+                image = "Images/DefaultIcon.png";
+
+            return new BitmapImage(new Uri($"pack://application:,,,/{image}"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WpfTreeview/MainWindow.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfTreeview
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //get every logical drive
+            foreach (var drive in Directory.GetLogicalDrives())
+            {
+                //create  a new item for it
+                var item = new TreeViewItem()
+                {
+                    //set the header and path
+                    Header = drive,
+                    //set the fullpath
+                    Tag = drive,
+                };
+
+                item.Items.Add(null);
+
+                //listen out for item being expanded
+                item.Expanded += Folder_Expanded;
+
+                FolderView.Items.Add(item);
+            }
+        }
+
+        private void Folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            var item = (TreeViewItem)sender;
+
+            //if the items only contains the dummy data
+            if (item.Items.Count != 1 || item.Items[0] != null)
+            {
+                return;
+            }
+
+            //Clear dummy data
+            item.Items.Clear();
+            var fullpPath = (string)item.Tag;
+
+
+            //create a blank list for directories
+            var dirictories = new List<string>();
+
+            //try and get directories from the folder
+            //ignoring any issues doing so
+            try
+            {
+                var dir = Directory.GetDirectories(fullpPath);
+
+                if (dir.Length > 0)
+                {
+                    dirictories.AddRange(dir);
+                }
+
+            }
+            catch //not best practice :P
+            {
+
+            }
+
+            dirictories.ForEach(directoriePath =>
+            {
+                var subItem = new TreeViewItem()
+                {
+                    Header = GetFileFolderName(directoriePath),
+                    Tag = directoriePath,
+                };
+
+                subItem.Items.Add(null);
+
+                subItem.Expanded += Folder_Expanded;
+
+                item.Items.Add(subItem);
+            });
+
+            //create a blank list for directories
+            var files = new List<string>();
+
+            //try and get directories from the folder
+            //ignoring any issues doing so
+            try
+            {
+                var fs = Directory.GetFiles(fullpPath);
+
+                if (fs.Length > 0)
+                {
+                    files.AddRange(fs);
+                }
+
+            }
+            catch //not best practice :P
+            {
+
+            }
+
+            files.ForEach(filePath =>
+            {
+                var subItem = new TreeViewItem()
+                {
+                    Header = GetFileFolderName(filePath),
+                    Tag = filePath,
+                };
+
+                item.Items.Add(subItem);
+            });
+
+        }
+
+        public static string GetFileFolderName(string directoryPath)
+        {
+            if (string.IsNullOrEmpty(directoryPath))
+            {
+                return string.Empty;
+            }
+
+            var normolizedPath = directoryPath.Replace('/', '\\');
+
+            var lastIndex = normolizedPath.LastIndexOf('\\');
+
+            if (lastIndex <= 0)
+            {
+                return directoryPath;
+            }
+            else
+            {
+                return normolizedPath.Substring(lastIndex + 1);
+            }
+        }
+    }
+}
