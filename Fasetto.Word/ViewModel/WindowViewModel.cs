3fe67a99db83aba0a0da022f509c6e62c393d44e
PolyakovSergey <ancientcult89@gmail.com>
2024-05-03 19:59:08 +0700

    lesson 4 complete

--- Fasetto.Word/ViewModel/WindowViewModel.cs
+++ Fasetto.Word/ViewModel/WindowViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using Fasetto.Word.ViewModels.Base;
+using System;
+using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Input;
 
 namespace Fasetto.Word.ViewModel
 {
@@ -9,7 +12,6 @@ namespace Fasetto.Word.ViewModel
 
         private int mOuterMarginSize = 10;
         private int mWindowRadius = 10;
-
 
         public int ResizeBorder { get; set; } = 6;
         public Thickness ResizeBorderThickness { get { return new Thickness(ResizeBorder + OuterMarginSize); } }
@@ -33,6 +35,9 @@ namespace Fasetto.Word.ViewModel
 
         public GridLength TitleHeightGridLength { get { return new GridLength(TitleHeight + ResizeBorder); } }
 
+        public double WindowMinimumWidth { get; set; } = 400;
+        public double WindowMinimumHeight { get; set; } = 400;
+
         public WindowViewModel(Window window)
         {
             mWindow = window;
@@ -45,6 +50,40 @@ namespace Fasetto.Word.ViewModel
                 OnPropertyChanged(nameof(WindowRadius));
                 OnPropertyChanged(nameof(WindowCornerRadius));
             };
+
+            MinimizeCommand = new RelayCommand(() => mWindow.WindowState = WindowState.Minimized);
+            MaximizeCommand = new RelayCommand(() => mWindow.WindowState ^= WindowState.Maximized);
+            CloseCommand = new RelayCommand(() => mWindow.Close());
+            MenuCommand = new RelayCommand(() => SystemCommands.ShowSystemMenu(mWindow, GetMousePoint()));
+
+            //Fix window resize issue
+            var resizer = new WindowResizer(mWindow);
         }
+
+        public ICommand MinimizeCommand { get; set; }
+        public ICommand MaximizeCommand { get; set;}
+        public ICommand CloseCommand { get; set;}
+        public ICommand MenuCommand { get; set;}
+
+
+        #region mouse helper
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetCursorPos(ref Win32Point pt);
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct Win32Point
+        {
+            public Int32 X;
+            public Int32 Y;
+        }
+
+        public static Point GetMousePoint()
+        {
+            Win32Point w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+            return new Point(w32Mouse.X, w32Mouse.Y);
+        }
+        #endregion
     }
 }
